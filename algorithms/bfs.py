05b0936f13098bdd9771ecadde8211bc4ed4803e
aurkaxi <abdullahalmuaz15@gmail.com>
2025-05-24 20:17:16 +0600

    Refactor BFS traversal function to correct type hints and improve implementation

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,22 +1,29 @@
-from queue import Queue
-from typing import Any
-
 from networkx import Graph
 
 
-def traverse(G: Graph, start_node: int) -> list[Any]:
-    visited = set()
-    q = Queue()
-    q.put(start_node)
-    order: list = []
+def traverse(G: Graph, start_node: int) -> list[int]:
+    """
+    Perform a depth-first search (DFS) traversal of the graph starting from the given node.
 
-    while not q.empty():
-        vertex = q.get()
+    Args:
+        G (Graph): The graph to traverse.
+        start_node (int): The starting node for the DFS traversal.
+
+    Returns:
+        list[int]: A list of nodes in the order they were visited during the DFS traversal.
+    """
+    visited = set()
+    queue = [start_node]
+    order: list[int] = []
+
+    while queue:
+        vertex = queue.pop(0)
         if vertex in visited:
             continue
         visited.add(vertex)
         order.append(vertex)
         for neighbor in G.neighbors(vertex):
             if neighbor not in visited:
-                q.put(neighbor)
+                queue.append(neighbor)
+
     return order
